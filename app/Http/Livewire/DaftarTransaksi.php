d3b1f9cc6ec7f33b3d0fa9d9d3107a64a65e24e4
devitaristy <devitaristiyanti084@gmail.com>
2023-01-20 10:50:05 +0700

    transasi-belom

---
+++ app/Http/Livewire/DaftarTransaksi.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+use App\Models\Transaksi;
+
+class DaftarTransaksi extends Component
+{
+
+    public function render()
+    {
+        return view('livewire.daftar-transaksi');
+    }
+}
---
+++ resources/views/livewire/daftar-transaksi.blade.php
@@ -0,0 +1,27 @@
+<div>
+    <div class="relative overflow-x-auto shadow-md sm:rounded-lg mt-5">
+        <table class="w-full text-sm text-left text-gray-600">
+            <thead class="text-xs text-gray-700 bg-gray-300">
+                <tr>
+                    <th scope="col" class="px-6 py-3">
+                        No
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Tanggal
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Jenis Tiket
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Jumlah
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Total
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+
+            </tbody>
+        </table>
+</div>
