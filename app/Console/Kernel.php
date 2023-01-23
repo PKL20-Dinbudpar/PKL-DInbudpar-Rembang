3e88e522d6bba254bec9a2828e02ea65b1695935
MKMaarif <maarif2910@students.undip.ac.id>
2023-01-23 09:59:02 +0700

    Create Rekap input schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,9 @@
 
 namespace App\Console;
 
+use App\Models\Rekap;
+use App\Models\Transaksi;
+use App\Models\Wisata;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -15,7 +18,29 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        // schedule input data rekap for each wisata daily
+        $schedule->call(function () {
+            $wisata = Wisata::all();
+            foreach ($wisata as $w) {
+                $rekap = new Rekap;
+                $rekap->tanggal = date('Y-m-d');
+                $rekap->id_wisata = $w->id_wisata;
+                $rekap->wisatawan_domestik = 0;
+                $rekap->wisatawan_mancanegara = 0;
+                $rekap->total_pendapatan = 0;
+                $rekap->save();
+            }
+        })->dailyAt('00:00');
+    }
+
+    /**
+     * Get the timezone that should be used by default for scheduled events.
+     *
+     * @return \DateTimeZone|string|null
+     */
+    protected function scheduleTimezone()
+    {
+        return 'Asia/Jakarta';
     }
 
     /**
